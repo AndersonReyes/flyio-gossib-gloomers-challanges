e10ac5a13594b12231a0a277c38b45259946f1cc
Anderson Reyes <reyesanderson428@gmail.com>
2023-05-27 11:37:39 -0400

    unique ids done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::io;
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
@@ -23,7 +24,16 @@ enum Body {
     },
 
     #[serde(rename = "init_ok")]
-    InitOk { in_reply_to: u64 },
+    InitOk { in_reply_to: u64, msg_id: u64 },
+
+    #[serde(rename = "generate")]
+    Generate { msg_id: u64 },
+    #[serde(rename = "generate_ok")]
+    GenerateOk {
+        id: String,
+        msg_id: u64,
+        in_reply_to: u64,
+    },
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -34,17 +44,24 @@ struct Message {
 }
 
 impl Message {
-    fn reply(&self) -> Message {
+    fn reply(&self, next_msg_id: u64) -> Message {
         let out_body = match &self.body {
             Body::Init { msg_id, .. } => Body::InitOk {
                 in_reply_to: *msg_id,
+                msg_id: next_msg_id,
             },
             Body::Echo { msg_id, echo } => Body::EchoOk {
-                msg_id: *msg_id,
+                msg_id: next_msg_id,
                 in_reply_to: *msg_id,
                 echo: echo.clone(),
             },
-            _ => panic!("noooo!"),
+            Body::Generate { msg_id } => Body::GenerateOk {
+                id: Uuid::new_v4().to_string(),
+                msg_id: next_msg_id,
+                in_reply_to: *msg_id,
+            },
+
+            _ => panic!("invalid message {:#?}", self),
         };
 
         Message {
@@ -55,18 +72,40 @@ impl Message {
     }
 }
 
-fn main() -> Result<()> {
-    loop {
-        let mut raw_msg = String::new();
+struct Node {
+    counter: u64,
+}
 
-        io::stdin()
-            .read_line(&mut raw_msg)
-            .expect("failed to read from stdin");
-
-        let msg: Message = serde_json::from_str(&raw_msg).unwrap();
-
-        println!("{}", serde_json::to_string(&msg.reply())?);
+impl Node {
+    pub fn new() -> Self {
+        Self { counter: 0 }
     }
 
+    fn gen_msg_id(&mut self) -> u64 {
+        self.counter += 1;
+        self.counter
+    }
+    pub fn run(&mut self) {
+        loop {
+            let mut raw_msg = String::new();
+
+            io::stdin()
+                .read_line(&mut raw_msg)
+                .expect("failed to read from stdin");
+
+            let msg: Message = serde_json::from_str(&raw_msg).unwrap();
+
+            println!(
+                "{}",
+                serde_json::to_string(&msg.reply(self.gen_msg_id()))
+                    .expect("Failed to serialize msg to string")
+            );
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let mut node = Node::new();
+    node.run();
     Ok(())
 }
